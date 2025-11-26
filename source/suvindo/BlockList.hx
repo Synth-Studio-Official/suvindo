a8fbcf52607318e070f8ff924be3a3bbfee86e9a
Viwu <sinconsistencia@gmail.com>
2025-11-26 15:06:18 -0600

    dont allow dupes

--- source/suvindo/BlockList.hx
+++ source/suvindo/BlockList.hx
@@ -10,23 +10,23 @@ using StringTools;
 
 class BlockList
 {
+	public static var BLOCK_LIST:Array<String> = [];
 
-    public static var BLOCK_LIST:Array<String> = [];
+	public static function reload()
+	{
+		BLOCK_LIST = [];
 
-    public static function reload()
-    {
-        BLOCK_LIST = [];
+		#if sys
+		for (image in ResourcePacks.readDirectory('images/blocks/'))
+		{
+			if (image.endsWith('.png') && !FileSystem.isDirectory(image))
+				if (!BLOCK_LIST.contains(Path.withoutExtension(Path.withoutDirectory(image))))
+					BLOCK_LIST.push(Path.withoutExtension(Path.withoutDirectory(image)));
+		}
+		#else
+		BLOCK_LIST = Assets.getText(ResourcePacks.getPath('data/blocks-list.txt')).split('\n');
+		#end
 
-        #if sys
-        for (image in ResourcePacks.readDirectory('images/blocks/'))
-        {
-            if (image.endsWith('.png') && !FileSystem.isDirectory(image))
-                BLOCK_LIST.push(Path.withoutExtension(Path.withoutDirectory(image)));
-        }
-        #else
-        BLOCK_LIST = Assets.getText(ResourcePacks.getPath('data/blocks-list.txt')).split('\n');
-        #end
-
-        trace('block list: ' + BLOCK_LIST);
-    }
+		trace('block list: ' + BLOCK_LIST);
+	}
 }
