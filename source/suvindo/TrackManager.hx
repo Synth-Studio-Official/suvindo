b70eedf2190c8159a40e75bf06e1845bb83b3ac3
Viwu <sinconsistencia@gmail.com>
2025-11-27 11:46:22 -0600

    reload tracks function

--- source/suvindo/TrackManager.hx
+++ source/suvindo/TrackManager.hx
@@ -1,8 +1,47 @@
 package suvindo;
 
+import haxe.io.Path;
+#if sys
+import sys.FileSystem;
+#end
+import suvindo.Requests.RequestsManager;
+
+using StringTools;
+
 class TrackManager
 {
-	public static var TRACKS:Array<String> = [];
+	public static var TRACKS_LIST:Array<String> = [];
 
-	public static function reload() {}
+	public static function reload()
+	{
+		TRACKS_LIST = [];
+		#if sys
+		var tracksDir:Array<String> = ResourcePacks.readDirectory('music/');
+		for (track in RequestsManager.ADD.tracks)
+		{
+			var path:String = ResourcePacks.getPath('music/' + track + '.png');
+			if (#if !sys Assets.exists #else FileSystem.exists #end (path))
+				tracksDir.push(path);
+		}
+		for (track in tracksDir)
+		{
+			var list_entry:String = Path.withoutExtension(Path.withoutDirectory(track));
+			var accept_entry = () ->
+			{
+				if (!TRACKS_LIST.contains(list_entry))
+					TRACKS_LIST.push(list_entry);
+			}
+
+			if (RequestsManager.REMOVE.tracks.contains(list_entry))
+				continue;
+
+			if (track.endsWith('.wav') && !FileSystem.isDirectory(track))
+				accept_entry();
+		}
+		#else
+		TRACKS_LIST = Assets.getText(ResourcePacks.getPath('data/tracks-list.txt')).split('\n');
+		#end
+
+		trace('tracks list: ' + TRACKS_LIST);
+	}
 }
